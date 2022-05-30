5780736d29410e2d6beac240f7b3e0b831f5779a
304HO <lonnie4165@gmail.com>
2022-05-30 16:15:37 +0900

    test

--- client/src/components/button.js
+++ client/src/components/button.js
@@ -1,17 +1,1 @@
-import styled from "styled-components";
 
-const Button_component = styled.div``;
-
-const Button = (props) => {
-  console.log(props);
-  return (
-    <button
-      className={`button-component ${props.className}`}
-      onClick={props.onClick}
-    >
-      {props.children}
-    </button>
-  );
-};
-
-export default Button;
