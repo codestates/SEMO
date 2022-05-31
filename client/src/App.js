e0bfd572fed377d3d73868fcf9261d4d79729ce1
bbammju <stwinsilver2@gmail.com>
2022-05-31 10:43:31 +0900

    vv

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import React from "react";
+import Footer from "./components/footer";
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <section className="features">
+        <Routes>
+          <Route path="/" element={<Footer/>}></Route>
+        </Routes>
+        </section>
+      </main>
+
     </div>
+    </BrowserRouter>
   );
 }
 
