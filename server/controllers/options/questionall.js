0ec3c3cea9c18e7650486a98d8cae88d89d0f030
bbammju <stwinsilver2@gmail.com>
2022-06-01 12:47:52 +0900

    questionall

--- server/controllers/options/questionall.js
+++ server/controllers/options/questionall.js
@@ -1,16 +1,19 @@
 const { question } = require('../../models');
 
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
 
 
   try{
-    const data = question.findAll({})
+    const data = await question.findAll({
+      attributes: ['*'],
+      raw: true
+    })
     if(!data) return res.status(404).send("invalid");
       
      else{
       if (data !== null) {
-        res.status(200).send({message: data});
+        res.status(200).json(data);
       } else {
         res.status(500).send('err');
       }
