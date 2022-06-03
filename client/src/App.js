9b9752242617364a83f6a30bef82e2067b5438ad
ikchan0625 <ikchan0625@gmail.com$ git config --global core.editor nanossh-keygen>
2022-06-03 15:15:48 +0900

    update route and update header

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,14 @@ import { Route, Routes, BrowserRouter } from "react-router-dom";
 import KakaoRedirectHandler from "./KakaoRedirectHandeler";
 import Mypage from "./pages/mypage";
 import Main from "./pages/main";
-
+import WritingPage from "./pages/question"
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<Main />} />
         <Route path="/mypage" element={<Mypage />} />
+        <Route path="/question/write" element={<WritingPage />} />
         <Route path="/oauth/callback/kakao" component={KakaoRedirectHandler} />
       </Routes>
     </BrowserRouter>
