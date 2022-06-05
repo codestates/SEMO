510e5f81dc66d73c635fde3363861d1b0356087d
bbammju <stwinsilver2@gmail.com>
2022-06-05 23:45:49 +0900

    v

--- server/index.js
+++ server/index.js
@@ -10,27 +10,33 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: ["http://localhost:3001"],
+    origin: ["http://localhost:3000"],
     credentials: true,
-    methods: ["GET", "POST", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "DELETE", "OPTIONS", "PATCH"],
   })
 );
 app.use(cookieParser());
-app.get("/signin", controllers.signin);
-app.post("/signup", controllers.signup);
-app.post("/signout", controllers.signout);
-app.post("/signwithdraw", controllers.signwithdraw);
+app.get("/sign/in", controllers.signin);
+app.post("/sign/up", controllers.signup);
+app.post("/sign/out", controllers.signout);
+app.delete("/sign/withdraw", controllers.signwithdraw);
 app.post("/question", controllers.question);
 app.post("/answer", controllers.answer);
-app.delete("/questiondelete", controllers.questiondelete);
-app.delete("/answerdelete", controllers.answerdelete);
-app.get("/questionall", controllers.questionall);
+app.delete("/question/delete", controllers.questiondelete);
+app.delete("/answer/delete", controllers.answerdelete);
+app.get("/question/all", controllers.questionall);
+app.get("/user/info", controllers.userinfo);
+app.patch("/answer/edit", controllers.answeredit);
+app.patch("/question/edit", controllers.questionedit);
+app.patch("/user/nickname/edit", controllers.nicknameedit);
+app.patch("/user/password/edit", controllers.passwordedit);
 
 const PORT = process.env.PORT || 3500;
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
+
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
