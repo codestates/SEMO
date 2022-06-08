f278ed8d3e9b6b72f881b717d22cec6c34fe85ee
304HO <lonnie4165@gmail.com>
2022-06-08 15:42:11 +0900

    refactoring state

--- client/src/pages/question.js
+++ client/src/pages/question.js
@@ -1,15 +1,15 @@
-
-import Button from "../components/button"
+import Button from "../components/button";
+import Dropdown from "../components/dropdownmenu";
 import Header from "../components/header";
 import WriteQuestionComponenet from "../components/writeQuestion";
 
-const WritingPage =() => { 
-     return (
-        <>
-          <Header></Header>
-          <WriteQuestionComponenet />
-        </>
-     )
-
-}
+const WritingPage = () => {
+  return (
+    <>
+      <Header></Header>
+      <Dropdown />
+      <WriteQuestionComponenet />
+    </>
+  );
+};
 export default WritingPage;
