2c569a39b8e77988831cee82fdf16779f673f125
bbammju <stwinsilver2@gmail.com>
2022-06-10 02:53:03 +0900

    v

--- server/controllers/users/id_check.js
+++ server/controllers/users/id_check.js
@@ -10,7 +10,7 @@ module.exports = async (req, res) => {
       where : {user_id : user_id}
     }); //일치하는 정보가 data 에 
     
-    if(!data) return res.status(200).send("okay");
+    if(data) return res.status(200).send("okay");
       
      else{
 
