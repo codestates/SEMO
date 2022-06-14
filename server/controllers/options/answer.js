1be1b85d73a7cfdb2cb98d6b7e91b9c51d765308
bbammju <stwinsilver2@gmail.com>
2022-06-14 23:02:19 +0900

    answerone

--- server/controllers/options/answer.js
+++ server/controllers/options/answer.js
@@ -5,14 +5,15 @@ module.exports = (req, res) => {
   // TODO: 회원가입 및 사용자 생성 로직을 작성하세요.
 
   try{
-    const {title, content, user_id} = req.body;
+    const {title, content, user_id, nickname} = req.body;
     if(!title || !content){
       return res.status(422).send("insufficient parameters supplied");
     } else {
        answer.create({
         title : title,
         content: content,
-        user_id: user_id
+        user_id: user_id,
+        nickname: nickname
       }).then(()=> {
         return res.status(201).json({message : "answer created"})
       })
