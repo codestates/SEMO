fad56822081a7a8c6b6d0b48a1f0c8bb7f3d4162
kwonms01 <kwonys02@naver.com>
2022-06-16 22:00:28 +0900

    add profile img

--- client/src/components/slickslide.js
+++ client/src/components/slickslide.js
@@ -5,7 +5,8 @@ import styled from 'styled-components';
 import axios from 'axios';
 import React from 'react';
 import { useState, useEffect } from 'react';
-
+import { Link } from "react-router-dom";
+import { useStore, useStoreTemp, useUserinfo } from "../zustand/store";
 
 const Container = styled.div`
   overflow:hidden;
@@ -43,19 +44,32 @@ const AnswerInfo = styled.div`
   align-items: flex-start;
   justify-content : center;  
 `
-
 const Name = styled.div`
   justify-content : center;
   font-size: 3vw;
   font-weight: bold;
 `
-
 const Title = styled.div`
   font-size: 0.8vw;
 `
+const DefaultProfileImg = require('../images/기본프로필이미지.png');
 
-const imgUrl = require('../images/권민수.jpg');
+function getRandomArr(arr) {
+  const result = []
 
+  if (!arr.length) {
+    return [3, 5, 4, 1, 0, 2, 10, 9, 8, 6, 7, 11];
+  }
+
+  while (result.length < 12) {
+    const randomNum = Math.floor(Math.random() * arr.length + 1) - 1;
+    if (!result.includes(randomNum)) {
+      result.push(randomNum)
+    }
+  }
+
+  return result;
+}//랜덤 배열을 리턴하는 함수
 
 function SimpleSlider() {
 
@@ -70,24 +84,6 @@ function SimpleSlider() {
     centerMode: false
   };
 
-  function getRandomIndex(arr) {
-    const result = []
-    if (!arr.length) {
-      return [3, 5, 4, 1, 0, 2];
-    }
-
-    while (result.length < 6) {
-      const randomNum = Math.floor(Math.random() * arr.length + 1) - 1;
-      if (!result.includes(randomNum)) {
-        result.push(randomNum)
-      }
-    }
-
-    return result;
-  }
-
-  const [questions, setquestions] = useState([]) // useEffect 안에서 전체 데이터를 받은후 .then을 빠져나와 6개로 추린 배열을 받는다.
-
   useEffect(() => {
     function getQuestInfo() {
       axios.get("http://localhost:3500/question/all", {
@@ -95,7 +91,7 @@ function SimpleSlider() {
         .then(res => {
 
           const data = res.data
-          const randomIndex = getRandomIndex(data); // [5, 1, 2, 4, 6, 0]
+          const randomIndex = getRandomArr(data); // [5, 1, 2, 4, 6, 0]
           const newQuestions = randomIndex.map(i => data[i])
           setquestions(newQuestions)
         })
@@ -105,18 +101,36 @@ function SimpleSlider() {
 
   }, []);
 
+  // setClickTitle: (a) =>
+  //   set((state) => ({ clickTitle: (state.clickTitle = a) }))
+
+  const [questions, setquestions] = useState([]) // useEffect 안에서 전체 데이터를 받은후 .then을 빠져나와 6개로 추린 배열을 받는다.
+  const { clickTitle, setClickTitle } = useStoreTemp();
+
+  const viewQuestion = (e) => {
+    setClickTitle(e);
+  };
+
   return (
     <Container>
-      <h5> 답변을 기다리고 있어요!</h5>
+
+      <Link to="/noticeboard">
+        <h5>답변을 기다리고 있어요!</h5>
+      </Link>
+
       <StyledSlider {...settings}>
         {questions.length && questions.map(item => {
           return (
             <div key={item.id}>
               <ImageContainer>
-                <ProfilePhoto src={item.url} />
+                <ProfilePhoto src={DefaultProfileImg} />
                 <AnswerInfo >
-                  <Name><div>{item.user_id} </div></Name>
-                  <Title><div>{item.title}</div></Title>
+                  <Name>
+                    {item.nickname}
+                  </Name>
+                  <Title onClick={() => { viewQuestion(item.title); }}>
+                    {item.title}
+                  </Title>
                 </AnswerInfo>
               </ImageContainer>
             </div>
