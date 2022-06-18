3344c87c13666ae33536dbb6f4dafff1c4ea5129
304HO <lonnie4165@gmail.com>
2022-06-18 22:17:05 +0900

    update darkmode

---
+++ client/src/style/styles.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const FlexContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+  padding: 0px 28px;
+  transition: all 0.2s linear;
+  width: calc(100%-2rem);
+
+  & h2 {
+    font-size: 48px;
+    font-weight: 500;
+    padding-bottom: 40px;
+    color: ${({ theme }) => theme.textColor};
+    text-align: center;
+  }
+`;
