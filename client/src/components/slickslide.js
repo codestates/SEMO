d1b39fb083af21b6062fac42f26f1e5b19fa54e1
kwonms01 <kwonys02@naver.com>
2022-06-20 02:00:25 +0900

    slide-mediaquery-update5

--- client/src/components/slickslide.js
+++ client/src/components/slickslide.js
@@ -139,7 +139,7 @@ function SimpleSlider() {
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 3,
-    // autoplay: true,
+    autoplay: true,
     autoplaySpeed: 2500,
     centerMode: false,
   };
